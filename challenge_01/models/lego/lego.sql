51a6b0249d053f3455ad1c2d21866855a180f497
le-luu <le.luu.dev@gmail.com>
2024-11-13 09:14:35 -0500

    Complete dbt Challenge 1

--- challenge_01/models/lego/lego.sql
+++ challenge_01/models/lego/lego.sql
@@ -1,12 +1,6 @@
 WITH UNIQUE_PARTS AS (
-SELECT 
-	P.part_num
-FROM parts as P
-INNER JOIN inventory_parts as IP on P.part_num = IP.part_num
-INNER JOIN inventories as I on I.id = IP.inventory_id
-INNER JOIN sets as S on S.set_num = I.set_num
-	GROUP BY P.part_num
-	HAVING COUNT(*) = 1
+SELECT *
+FROM {{ ref('unique_parts') }}
 )
 SELECT 
 	T.name as theme_name,
